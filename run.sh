1e816b00de66eff7efcc1821bd64084a8c73b0aa
exeqdev <39518154+exeqdev@users.noreply.github.com>
2018-09-04 09:20:35 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#get external IP for passive
+dig +short myip.opendns.com @resolver1.opendns.com | tee /etc/pure-ftpd/conf/ForcePassiveIP
+
+#get flags from configuration wrapper
+PURE_FTPD_FLAGS="$(/usr/sbin/pure-ftpd-wrapper -s)"
+
+# let users know what flags we've ended with (useful for debug)
+echo "Starting Pure-FTPd:"
+echo "  pure-ftpd $PURE_FTPD_FLAGS"
+
+# start pureftpd
+exec /usr/sbin/pure-ftpd $PURE_FTPD_FLAGS
